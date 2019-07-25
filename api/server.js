cb15378301ec71540835a4672341eeb76e4ba87a
erozonachi <erozonachi.james@gmail.com>
2019-07-25 23:09:06 +0100

    Add countries post route

---
+++ api/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const Countries = require('../countries/CountriesModel');
+
+const server = express();
+
+server.use(express.json());
+
+server.post('/countries', (req, res) => {
+  Countries.insert(req.body)
+    .then(country => {
+      res.status(201).json(country);
+    })
+    .catch(error => {
+      const err = {
+        message: error.message,
+        stack: error.stack,
+      };
+      res.status(500).json(err);
+    });
+});
+
+module.exports = server;
