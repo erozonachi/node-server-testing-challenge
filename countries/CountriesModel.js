841e89ddfdd91d4443613b21d9da3da00b258fdc
erozonachi <erozonachi.james@gmail.com>
2019-07-25 22:38:46 +0100

    Add logic to the model insert function

--- countries/CountriesModel.js
+++ countries/CountriesModel.js
@@ -2,12 +2,14 @@ const db = require('../data/dbConfig');
 
 module.exports = {
   insert: function(country) {
-
+    return db('countries')
+      .insert(country)
+      .then(([ id ]) => this.get(id));
   },
   get: function(id) {
 
   },
   delete: function(id) {
-    
+
   }
 };
