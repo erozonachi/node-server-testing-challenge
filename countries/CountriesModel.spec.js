dae3a41684b7945626ac7fa2e155c049aabbc236
erozonachi <erozonachi.james@gmail.com>
2019-07-25 22:33:37 +0100

    Add country deletion spec

--- countries/CountriesModel.spec.js
+++ countries/CountriesModel.spec.js
@@ -35,3 +35,20 @@ describe('Countries.insert', () => {
     expect(country.name).toBe('Nigeria');
   });
 });
+
+describe('Countries.delete', () => {
+  it('can delete a country from the db!', async () => {
+    let countries = await Countries.get();
+    expect(countries).toHaveLength(0);
+
+    await Countries.insert({ name: 'Nigeria', capital: 'Abuja', population: 150000000 });
+    await Countries.insert({ name: 'Ghana', capital: 'Accra', population: 30000000 });
+    countries = await Countries.get();
+    expect(countries).toHaveLength(2);
+
+    await Countries.delete(countries[0].id);
+    await Countries.delete(countries[1].id);
+    countries = await Countries.get();
+    expect(countries).toHaveLength(0);
+  });
+});
